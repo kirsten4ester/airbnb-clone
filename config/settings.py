ab46a065ce1a59c5a092702ef10a16d9b937db3c
dev-jaesim <dev.jaesim@gmail.com>
2019-10-31 13:08:20 +0900

    email verification done

--- config/settings.py
+++ config/settings.py
@@ -131,3 +131,11 @@ AUTH_USER_MODEL = "users.User"
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 
 MEDIA_URL = "/media/"
+
+
+# Email Configuration
+EMAIL_HOST = "smtp.mailgun.org"
+EMAIL_PORT = "587"
+EMAIL_HOST_USER = os.environ.get("MAILGUN_USERNAME")
+EMAIL_HOST_PASSWORD = os.environ.get("MAILGUN_PASSWORD")
+EMAIL_FROM = "airbnb-admin@sandboxeedd4ca787914755a527871dc70c08a1.mailgun.org"
